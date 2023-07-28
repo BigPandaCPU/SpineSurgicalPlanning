1817799c4c2be3d25e1f48168b92bc6c8bdac730
dufei <dkb@hhrbrobot.com>
2023-07-28 18:04:35 +0800

    change:修改由于x轴反向导致的检测框不匹配的问题

--- SpineSurgicalPlanning.py
+++ SpineSurgicalPlanning.py
@@ -19,6 +19,7 @@ import torch
 import time
 from tqdm import tqdm
 import nibabel as nib
+import SimpleITK as sitk
 
 from utils.file_operations import createSpinePlanningPropFile, createSpineHurFile
 
@@ -218,7 +219,7 @@ def spineSeg(input_dcm_dir, output_folder):
 
     pir_img_all_Z, pir_img_all_Y, pir_img_all_X = pir_img_all.shape
 
-    pir_img = pir_img_all[y_min_new:y_max_new, pir_img_all_Y - z_max_new:pir_img_all_Y - z_min_new, x_min_new:x_max_new]
+    pir_img = pir_img_all[y_min_new:y_max_new, pir_img_all_Y - z_max_new:pir_img_all_Y - z_min_new, pir_img_all_Z-x_max_new:pir_img_all_Z - x_min_new]
     print(' ... loaded CT volume in isotropic resolution and PIR orientation ')
     time_load_end = time.time()
 
@@ -272,7 +273,7 @@ def spineSeg(input_dcm_dir, output_folder):
 
     mask = np.zeros_like(pir_img_all)
     mask[y_min_new:y_max_new, pir_img_all_Y - z_max_new:pir_img_all_Y - z_min_new,
-    x_min_new:x_max_new] = multi_label_mask
+    pir_img_all_Z-x_max_new:pir_img_all_Z - x_min_new] = multi_label_mask
 
     mask = get_result_of_mask(mask, ori_orient_code, ori_spacing, ori_size, ori_aff, save_folder, scanname)
 
@@ -302,159 +303,24 @@ if __name__ == "__main__":
     start_label = args.start_spine_label
     end_label = args.end_spine_label
 
-    input_dcm_dir = "/media/alg/data3/DeepSpineData/spine_test/Test10/DICOM"
-    save_dir = "/media/alg/data3/DeepSpineData/spine_test/Test10/predict_spine"
+    input_dcm_dir = "/media/alg/data3/DeepSpineData/spine_test/Test13/DICOM"
+    save_dir = "/media/alg/data3/DeepSpineData/spine_test/Test13/predict_spine"
     mask, dicom_info = spineSeg(input_dcm_dir, save_dir)
+
+    mask_itk = sitk.GetImageFromArray(mask)
+    mask_itk.SetOrigin(dicom_info.origin)
+    mask_itk.SetSpacing(dicom_info.spacing)
+    mask_itk.SetDirection(dicom_info.direction)
+    save_mask_file = os.path.join(save_dir, "CT_0000_seg.nii.gz")
+    sitk.WriteImage(mask_itk, save_mask_file)
 
     save_hur_dir = os.path.join(save_dir, "hur")
     os.makedirs(save_hur_dir, exist_ok=True)
-    removeAllFiles(save_hur_dir )
+    removeAllFiles(save_hur_dir)
 
-    createSpinePlanningPropFile(mask, dicom_info, save_hur_dir, start_label="T8", end_label="L5")
+    createSpinePlanningPropFile(mask, dicom_info, save_hur_dir)
 
     patient_id = dicom_info.patientID
     createSpineHurFile(save_hur_dir, save_dir, patient_id)
 
 
-
-
-
-    # import argparse, os, glob, sys
-    # from SpineSeg.utils import mkpath, read_isotropic_pir_img_from_nifti_img,write_result_to_file
-    # import numpy as np
-    # import torch
-    # import time
-    # from tqdm import tqdm
-    # import nibabel as nib
-    # torch.set_grad_enabled(False)
-    #
-    # import SimpleITK as sitk
-    #
-    #
-    # parser = argparse.ArgumentParser(description='Run pipeline on a single CT scan.')
-    # parser.add_argument('-D', '--input_data', type=str, help='a CT scan or a folder of CT scans in nifti format')
-    # parser.add_argument('-S', '--save_folder',  default='-1', type=str, help='folder to save the results')
-    # parser.add_argument('-F', '--force_recompute', action='store_true', help='set True to recompute and overwrite the results')
-    # parser.add_argument('-L', '--initial_locations', action='store_true', help='set True to use initial location predictions')
-    # parser.add_argument('-Ns', '--seg_spine_norm', action='store_true', help='set True to use normalized spine segmentor')
-    # parser.add_argument('-Nv', '--seg_vert_norm', action='store_true', help='set True to use normalized vertebra segmentor')
-    # args = parser.parse_args()
-    #
-    #
-    # src_dir = "/media/alg/data3/DeepSpineData/CTSpine1k_new/image/good/"
-    # save_dir = "/media/alg/data3/DeepSpineData/CTSpine1k_new/image/predict_verSeg"
-    #
-    # img_names = os.listdir(src_dir)
-    # for img_name in tqdm(img_names):
-    #     time_sum_start = time.time()
-    #     shot_name = img_name[:-7]
-    #     save_folder = os.path.join(save_dir, shot_name)
-    #     #
-    #     if "Test" in img_name:
-    #         continue
-    #     ### results saving locations
-    #     #save_folder = args.save_folder
-    #     if save_folder != '-1':
-    #         mkpath(save_folder)
-    #     else:
-    #         current_path = os.path.abspath(os.getcwd())
-    #         save_folder = os.path.join(current_path, 'results')
-    #         mkpath(save_folder)
-    #
-    #     scanname = shot_name
-    #     input_file = os.path.join(src_dir, img_name)
-    #     print(' ... starting to process: ', input_file)
-    #
-    #
-    #     img_data, ori_size, ori_spacing, ori_orient_code, ori_aff, crop_box, dicom_info = getSpineBBoxFromImage(input_file, save_folder)
-    #     x_min, x_max, y_min, y_max, z_min, z_max = crop_box
-    #     img_data_trans = img_data.transpose(2,1,0)
-    #
-    #     nifti_img = nib.Nifti1Image(img_data_trans, ori_aff)
-    #
-    #     # =================================================================
-    #     # Load the CT scan
-    #     # =================================================================
-    #
-    #     ### TODO: data I/O for other formats
-    #
-    #     time_load_start = time.time()
-    #     try:
-    #         pir_img_all = read_isotropic_pir_img_from_nifti_img(nifti_img, ori_spacing, ori_orient_code)
-    #     except ImageFileError:
-    #         sys.exit('The input CT should be in nifti format.')
-    #
-    #     y_min_new = int(y_min*ori_spacing[1])
-    #     y_max_new = int(y_max*ori_spacing[1])
-    #     z_min_new = int(z_min*ori_spacing[2])
-    #     z_max_new = int(z_max*ori_spacing[2])
-    #     x_min_new = int(x_min*ori_spacing[0])
-    #     x_max_new = int(x_max*ori_spacing[0])
-    #
-    #     pir_img_all_Z, pir_img_all_Y, pir_img_all_X = pir_img_all.shape
-    #
-    #     pir_img = pir_img_all[y_min_new:y_max_new, pir_img_all_Y-z_max_new:pir_img_all_Y-z_min_new, x_min_new:x_max_new]
-    #     print(' ... loaded CT volume in isotropic resolution and PIR orientation ')
-    #     time_load_end = time.time()
-    #
-    #     # =================================================================
-    #     # Spine binary segmentation
-    #     # =================================================================
-    #
-    #     models = load_models(seg_spine_norm=False, seg_vert_norm=False)
-    #
-    #     time_binary_start = time.time()
-    #     from SpineSeg.segment_spine import binary_segmentor
-    #     binary_mask = binary_segmentor(pir_img, models['seg_binary'], mode='overlap', norm=args.seg_spine_norm)
-    #     time_binary_end = time.time()
-    #
-    #     print(' ... obtained spine binary segmentation ')
-    #
-    #     # =================================================================
-    #     # Initial locations
-    #     # =================================================================
-    #     time_locate_start = time.time()
-    #     locations = np.array([])
-    #     #if args.initial_locations:
-    #     from SpineSeg.locate import locate
-    #     locations = locate(pir_img, models['loc_sagittal'], models['loc_coronal'])
-    #     time_locate_end = time.time()
-    #     print(' ... obtained {} initial 3D locations '.format(len(locations)))
-    #
-    #     # =================================================================
-    #     # Consistency circle - Locations refine - multi label segmentation
-    #     # =================================================================
-    #
-    #     time_con_start = time.time()
-    #     from SpineSeg.consistency_loop import consistency_refinement_close_loop
-    #
-    #     multi_label_mask, locations, labels, loc_has_converged = consistency_refinement_close_loop(locations, pir_img, binary_mask,
-    #                                                                         models['seg_individual'], args.seg_vert_norm,
-    #                                                                         models['id_group'], models['id_cervical'],
-    #                                                                         models['id_thoracic'], models['id_lumbar'])
-    #
-    #     print("\nfinall loc:", locations)
-    #
-    #     time_con_end = time.time()
-    #     print(' ... obtained PIR multi label segmentation ')
-    #
-    #     # =================================================================
-    #     # Save the result in original format
-    #     # =================================================================
-    #     time_save_start = time.time()
-    #
-    #     mask = np.zeros_like(pir_img_all)
-    #     mask[y_min_new:y_max_new, pir_img_all_Y-z_max_new:pir_img_all_Y-z_min_new, x_min_new:x_max_new] = multi_label_mask
-    #
-    #     write_result_to_file(mask, ori_orient_code, ori_spacing, ori_size, ori_aff, save_folder, scanname)
-    #     time_end = time.time()
-    #
-    #     time_save_end = time.time()
-    #     time_sum_end = time.time()
-    #     print("\n\nload img time:", time_load_end - time_load_start)
-    #     print("locate time:", time_locate_end - time_locate_start)
-    #     print("binary seg time:", time_binary_end - time_binary_start)
-    #     print("con time:", time_con_end - time_con_start)
-    #     print("save time:", time_save_end - time_save_start)
-    #     print("sum time:", time_sum_end - time_sum_start)
-    #     print("\n\n\n")
